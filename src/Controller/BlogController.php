ecc73a643929fb332d6d9a77fb36b3699e177c04
Drumont Erick <drumont06@gmail.com>
2018-10-28 22:34:26 +0100

    Setting up Symfony

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticleRepository $repository)
+    {
+        $articles = $repository->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue dans ce Blog",
+            'age' => 21
+        ]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $request, ObjectManager $manager){
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                    ->add('title',TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Titre de l'article",
+                            'class' => "form-control"
+                        ]
+                    ])
+                    ->add('content',TextareaType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenu",
+                            'class' => "form-control"
+                        ]
+                    ])
+                    ->add('image',TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenu",
+                            'class' => "form-control"
+                        ]
+                    ])
+                    ->getForm();
+
+        return $this->render('blog/create.html.twig',  [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article){
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+}
