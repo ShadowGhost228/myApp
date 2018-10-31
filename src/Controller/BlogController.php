9bb89bc6e674faf9c44ff491f10e8af53f815c71
Drumont Erick <drumont06@gmail.com>
2018-10-31 19:48:51 +0100

    Comment update

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -71,9 +73,24 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(Article $article){
+    public function show(Article $article, Request $request, ObjectManager $manager){
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
         return $this->render('blog/show.html.twig', [
-            'article' => $article
+            'article' => $article,
+            'formComment' => $form->createView()
         ]);
     }
 
